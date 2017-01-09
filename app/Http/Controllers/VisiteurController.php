185862acef20e5c1ea165124283fbfe8d36782d4
Rudy <Rudy@DESKTOP-FLNU10E.sglo.priv>
2017-01-09 16:17:45 +0100

    Systeme connexion/deconnexion effectif

--- app/Http/Controllers/VisiteurController.php
+++ app/Http/Controllers/VisiteurController.php
@@ -3,9 +3,7 @@
 
 
 namespace App\Http\Controllers;
-use App\metier\Produit;
-use App\metier\Marque;
-use App\metier\Type;
+use App\metier\Visiteur;
 use Request;
 use Illuminate\Support\Facades\Session;
 use Exception;
@@ -28,140 +26,160 @@ class VisiteurController extends Controller {
         }
     }
     
-    // Lister tout les produits que l'ont dispose sans caractéristique précise
-    public function getListeProduit(){
-        // Création d'un objet Produit avec appel d'une méthode dans la classe
-        $unProduit= new Produit();
-        $mesProduits = $unProduit->getListeProduit();
-        return view('tableauProduit', compact('mesProduits'));
+    public function verifDispoCompte(){
+        $login = Request::input('login');
+        $unVisiteur = new Visiteur;
+        $desVisiteurs = $unVisiteur->verifDispo($login);
+        return view('/signIn', compact ('desVisiteurs'));
     }
     
-    //Supprimer un produit lié à son id
-    public function deleteProduit($id_vet){
-        $unManga = new Produit();
-        $unManga->deleteProduit($id_vet);
-        return redirect('/listerProduit');
+    public  function getLogin(){
+        $erreur = "";
+        return view('formLogin', compact('erreur'));
     }
     
-    // Liste les produits relatifs aux hommes
-    public function getListeProduitHomme(){
-        $unProduit= new Produit();
-        $mesProduits = $unProduit->getListeProduitHomme();
-        return view('tableauProduit', compact('mesProduits'));
+    public function signOut(){
+        //Création d'un objet, et appel d'une méthode pour cette objet
+        $unVisteur = new Visiteur();
+        $unVisteur->logout();
+        return view('accueil');
     }
-    
-    //Lister les produits relatif aux femmes
-    public function getListeProduitFemme(){
-        $unProduit = new Produit();
-        $mesProduits = $unProduit->getListeProduitFemme();
-        return view('tableauProduit', compact('mesProduits'));
-    }
-    
-    //Lister les produits relatif aux enfants
-    public function getListeProduitEnfant(){
-        $unProduit = new Produit();
-        $mesProduits = $unProduit->getListeProduitEnfant();
-        return view('tableauProduit', compact('mesProduits'));
-    }
-    
-    //Permet d'appeler le formulaire d'ajout produit avec les liste de type/marque disponible
-    public function ajoutProduit(){
-        $uneMarque = new Marque();
-        $mesMarques = $uneMarque->getListeGenres();
-        $unType = new Type();
-        $mesTypes = $unType->getListeScenaristes();
-        return view('formProduit', compact('mesMarques','mesTypes'));
-    }
-    
-    //Permet d'envoyer en base le formulaire d'ajout d'un produit
-    public function postajouterProduit(){
-        //Récupération d'une valeur dans un input
-        $nom_vet = Request::input('nom_vet');
-        $couleur = Request::input('couleur');
-        $taille = Request::input('taille');
-        $id_type = Request::input('id_type');
-        $id_marque = Request::input('id_marque');
-        $prix = Request::input('prix');
-        $qteStock = Request::input('qteStock');
-        $image = Input::file('couverture');
-        $unManga = new Produit();
-        if($image){
-            $nomImage=$image->getClientOriginalName();
-            $image->move(public_path("/assets/image/"),$nomImage);
-            $unManga->ajoutMangaImage($nom_vet, $couleur, $taille, $id_type, $id_marque, $prix, $qteStock, $nomImage);
-        }
-        else{
-            $unManga->ajoutManga($nom_vet, $couleur, $taille, $id_type, $id_marque, $prix, $qteStock);
-        }
-       
-        return view('home');
-    }
-    
-    //Permet d'appeler le formulaire pour modifier un produit relatif à un id de vetement
-        public function modifierProduit($id_vet){
-        $unProd = new Produit();
-        $unProduit = $unProd->getProduit($id_vet);
-        $uneMarque = new Marque();
-        $mesMarques = $uneMarque->getListeGenres();
-        $unType = new Type();
-        $mesTypes = $unType->getListeScenaristes();
-        return view('formProduitModif', compact('unProduit', 'mesMarques', 'mesTypes'));
-    }
-    
-    //Envoie en base les information du formulaire de modif d'un produit
-    public function postmodifierProduit($id = null){
-        $code = $id;
-        $nom_vet = Request::input('nom_vet');
-        $code_type = Request::input('id_type');
-        $code_marque = Request::input('id_marque');
-        $qteStock = Request::input('qteStock');
-        $prix = Request::input('prix');
-        $couleur = Request::input('couleur');
-        $taille = Request::input('taille');
-        $unProduit = new Produit();
-        $unProduit->modificationProduit($code, $nom_vet, $code_type,
-                $code_marque, $qteStock,
-                $prix, $couleur, $taille);
-        $mesProduits = $unProduit->getListeProduit();
-        return view('welcome');
-    }
-    
-    //Lister les marque disponible sur la boutique
-    public function ListerMarque(){
-        $uneMarque = new Marque();
-        $mesMarques = $uneMarque->getListeGenres();
-        return view('formProduitGenre', compact('mesMarques'));
-    }
-    
-    //Permet d'afficher les 
-    public function postAfficherMangaGenre(){
-        $code_marque = Request::input('cbGenres');
-        $lib_marque = Request::input('cbGenres');
-        $unProduit = new Produit();
-        $mesProduits = $unProduit->getListeMangaGenre($code_marque);
-        return view('pageMangaGenre', compact('mesProduits','lib_marque'));
-    }
-    
-    //Permet d'ajouter une marque dans la base
-    public function ajoutMarque(){
-        return view('/formAjoutMarque');
-    }
-    
-    //Validation des infos entrer dans le formulaire ajout marque
-    public function postAjoutMarque(){
-        $lib_marque = Request::input('nom_marque');
-        $uneMarque = new Marque();
-        $uneMarque->ajoutMarque($lib_marque); 
-        return view('welcome');
-    }
-    
-    public function postAfficherProduitGenre(){
-        $code_marque = Request::input('cbGenres');
-        $unProduit = new Produit();
-        $mesProduits = $unProduit->getListeMangaGenre($code_marque);
-        return view('pageMangaGenre', compact('mesProduits'));
-    }
-    
+//    
+//    
+//    // Lister tout les produits que l'ont dispose sans caractéristique précise
+//    public function getListeProduit(){
+//        // Création d'un objet Produit avec appel d'une méthode dans la classe
+//        $unProduit= new Produit();
+//        $mesProduits = $unProduit->getListeProduit();
+//        return view('tableauProduit', compact('mesProduits'));
+//    }
+//    
+//    //Supprimer un produit lié à son id
+//    public function deleteProduit($id_vet){
+//        $unManga = new Produit();
+//        $unManga->deleteProduit($id_vet);
+//        return redirect('/listerProduit');
+//    }
+//    
+//    // Liste les produits relatifs aux hommes
+//    public function getListeProduitHomme(){
+//        $unProduit= new Produit();
+//        $mesProduits = $unProduit->getListeProduitHomme();
+//        return view('tableauProduit', compact('mesProduits'));
+//    }
+//    
+//    //Lister les produits relatif aux femmes
+//    public function getListeProduitFemme(){
+//        $unProduit = new Produit();
+//        $mesProduits = $unProduit->getListeProduitFemme();
+//        return view('tableauProduit', compact('mesProduits'));
+//    }
+//    
+//    //Lister les produits relatif aux enfants
+//    public function getListeProduitEnfant(){
+//        $unProduit = new Produit();
+//        $mesProduits = $unProduit->getListeProduitEnfant();
+//        return view('tableauProduit', compact('mesProduits'));
+//    }
+//    
+//    //Permet d'appeler le formulaire d'ajout produit avec les liste de type/marque disponible
+//    public function ajoutProduit(){
+//        $uneMarque = new Marque();
+//        $mesMarques = $uneMarque->getListeGenres();
+//        $unType = new Type();
+//        $mesTypes = $unType->getListeScenaristes();
+//        return view('formProduit', compact('mesMarques','mesTypes'));
+//    }
+//    
+//    //Permet d'envoyer en base le formulaire d'ajout d'un produit
+//    public function postajouterProduit(){
+//        //Récupération d'une valeur dans un input
+//        $nom_vet = Request::input('nom_vet');
+//        $couleur = Request::input('couleur');
+//        $taille = Request::input('taille');
+//        $id_type = Request::input('id_type');
+//        $id_marque = Request::input('id_marque');
+//        $prix = Request::input('prix');
+//        $qteStock = Request::input('qteStock');
+//        $image = Input::file('couverture');
+//        $unManga = new Produit();
+//        if($image){
+//            $nomImage=$image->getClientOriginalName();
+//            $image->move(public_path("/assets/image/"),$nomImage);
+//            $unManga->ajoutMangaImage($nom_vet, $couleur, $taille, $id_type, $id_marque, $prix, $qteStock, $nomImage);
+//        }
+//        else{
+//            $unManga->ajoutManga($nom_vet, $couleur, $taille, $id_type, $id_marque, $prix, $qteStock);
+//        }
+//       
+//        return view('home');
+//    }
+//    
+//    //Permet d'appeler le formulaire pour modifier un produit relatif à un id de vetement
+//        public function modifierProduit($id_vet){
+//        $unProd = new Produit();
+//        $unProduit = $unProd->getProduit($id_vet);
+//        $uneMarque = new Marque();
+//        $mesMarques = $uneMarque->getListeGenres();
+//        $unType = new Type();
+//        $mesTypes = $unType->getListeScenaristes();
+//        return view('formProduitModif', compact('unProduit', 'mesMarques', 'mesTypes'));
+//    }
+//    
+//    //Envoie en base les information du formulaire de modif d'un produit
+//    public function postmodifierProduit($id = null){
+//        $code = $id;
+//        $nom_vet = Request::input('nom_vet');
+//        $code_type = Request::input('id_type');
+//        $code_marque = Request::input('id_marque');
+//        $qteStock = Request::input('qteStock');
+//        $prix = Request::input('prix');
+//        $couleur = Request::input('couleur');
+//        $taille = Request::input('taille');
+//        $unProduit = new Produit();
+//        $unProduit->modificationProduit($code, $nom_vet, $code_type,
+//                $code_marque, $qteStock,
+//                $prix, $couleur, $taille);
+//        $mesProduits = $unProduit->getListeProduit();
+//        return view('welcome');
+//    }
+//    
+//    //Lister les marque disponible sur la boutique
+//    public function ListerMarque(){
+//        $uneMarque = new Marque();
+//        $mesMarques = $uneMarque->getListeGenres();
+//        return view('formProduitGenre', compact('mesMarques'));
+//    }
+//    
+//    //Permet d'afficher les 
+//    public function postAfficherMangaGenre(){
+//        $code_marque = Request::input('cbGenres');
+//        $lib_marque = Request::input('cbGenres');
+//        $unProduit = new Produit();
+//        $mesProduits = $unProduit->getListeMangaGenre($code_marque);
+//        return view('pageMangaGenre', compact('mesProduits','lib_marque'));
+//    }
+//    
+//    //Permet d'ajouter une marque dans la base
+//    public function ajoutMarque(){
+//        return view('/formAjoutMarque');
+//    }
+//    
+//    //Validation des infos entrer dans le formulaire ajout marque
+//    public function postAjoutMarque(){
+//        $lib_marque = Request::input('nom_marque');
+//        $uneMarque = new Marque();
+//        $uneMarque->ajoutMarque($lib_marque); 
+//        return view('welcome');
+//    }
+//    
+//    public function postAfficherProduitGenre(){
+//        $code_marque = Request::input('cbGenres');
+//        $unProduit = new Produit();
+//        $mesProduits = $unProduit->getListeMangaGenre($code_marque);
+//        return view('pageMangaGenre', compact('mesProduits'));
+//    }
+//    
 
     
    
---
+++ app/metier/Visiteur.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\metier;
+
+use Illuminate\Support\Facades\Session;
+use Illuminate\Database\Eloquent\Model;
+use DB;
+
+class Visiteur extends Model {
+
+    //
+    protected $table = 'visiteur';
+    public $timestamps = false;
+    protected $fillable = [
+        'idVis',
+        'login',
+        'mdpVis',
+        'nomVis',
+        'prenomVis',
+        'adresseVis',
+        'mailVis',
+        'telVis',
+        'villeVis',
+        'codePostVis',
+        'ageVis',
+        'ncptVis'
+    ];
+    
+    public function login($login, $pwd) {
+        $connected = false;
+        $visiteur = DB::table('visiteur')
+                ->select()
+                ->where('login', '=', $login)
+                ->first();
+        if ($visiteur) {
+            if ($visiteur->mdpVis == $pwd) {
+                Session::put('id', $visiteur->idVis);
+                Session::put('prenom', $visiteur->prenomVis);
+                $connected = true;
+            }
+        }
+        return $connected;
+    }
+    
+    public function logout() {
+        Session::put('id', 0);
+        Session::put('nom', 0);
+    }
+}
