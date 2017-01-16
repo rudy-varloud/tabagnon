ce4ebc4db579b258c4f052afc7647c6b2399a6fd
sio <sio@DESKTOP-GSEDVBB.sglo.priv>
2017-01-16 16:05:14 +0100

    ajout script visite (guide)

--- resources/views/formAjoutVisite.blade.php
+++ resources/views/formAjoutVisite.blade.php
@@ -2,74 +2,83 @@
 @section('content')
 {!! Form::open(['url' => 'postFormVisite', 'files' => true]) !!}
 <script>
-    function verifGuide(){
-    var guide_select = $('.nomGuideVisite option:selected').val();
-    var guide_input = $('.nomGuideVisMan').val();
-    
-    alert(guide_input);
-    alert(guide_select);
-}
+    function verifGuide()
+            var test = $('.nomGuideVis').val();
+    var test2 = $('.nomGuideVisMan').val();
+    if ((test2 === '') && (test !== null)) {
+        alert('bonjour');
+        $('.nomGuideVisMan').removeAttr('required');
+        $('.nomGuideVis').attr('required');
+    } else if ((test === null) && (test2 !== '')) {
+        alert('Skurtskurt');
+        $('.nomGuideVis').removeAttr('required');
+        $('.nomGuideVisMan').attr('required');
+    } else {
+        alert('Vous ne pouvez pas remplir les deux champs de guide');
+        }
+    }
 </script>
 <body>
-<div class="col-lg-12 col-md-12 col-s-12 box">
-    <center><h2 class='formVisite'> Créer une visite </h2></center>
-    <!--    <div class="form-horizontal">-->
-    <!--        <div class="form-control">-->
-    <div class="form-group">
-        <label class="col-md-3 control-label">Nom de la visite: </label>
-        <input name="nomVisite" class="form-control" type="text" value="{{$titreArticle or ''}}" placeholder="Titre de l'aticle" required autofocus>
-    </div>
-    <br>
-    <div class="form-group">
-        <label class="col-md-3 control-label">Lieu de la visite:</label>
-        <input name="lieuxVisite" class="form-control" type="text" placeholder="Lieux de la visite" required>
-    </div>
-    <div class="form-group">
-        <label class="col-md-3 control-label">Description de la visite: </label>
-        <input name='description' class="form-control" type="text" value="{{$description or ''}}" placeholder="Description de l'article" required autofocus>
-    </div>
-    <br>
-    <div class='form-group'>
-        <label class="col-md-3 control-label"> Date de la visite: </label>
-        <input name="date" class="form-control" type="date" value="" placeholder="Date et heure souhaitez pour la visite" required>
-    </div>
-    <br>
-    <div class="form-group">
-        <label class='col-md-3 control-label'>Prix de la visite: </label>
-        <input name='prix' class="form-control" type="number" placeholder="Prix de la visite" required>
-    </div>
-    <br>
-    <div class="form-group">
-        <label class="col-md-4 control-label">Nombre de place(s) pour la visite: </label>
-        <input name="nbPlace" class="form-control" type="number" placeholder="Nombre de place dispo pour cette visite" required>
-    </div>
-    <br>
-    <div class='form-group'>
-        <label class='col-md-4 control-label'>Sélectionnez le guide pour cette visite: </label>
-        <select name="nomGuideVisite" class="form-control nomGuideVis">
-            <option value="Selectionnez le guide souhaité" selected disabled required>Selectionnez le guide souhaité</option>
-            @foreach($mesVisiteurs as $unVisiteur)
-            <option value="{{$unVisiteur -> idVis}}">{{$unVisiteur->prenomVis}} {{$unVisiteur->nomVis}}</option>
-            @endforeach
-        </select>
+    <div class="col-lg-12 col-md-12 col-s-12 box">
+        <center><h2 class='formVisite'> Créer une visite </h2></center>
+        <!--    <div class="form-horizontal">-->
+        <!--        <div class="form-control">-->
+        <div class="form-group">
+            <label class="col-md-3 control-label">Nom de la visite: </label>
+            <input name="nomVisite" class="form-control" type="text" value="{{$titreArticle or ''}}" placeholder="Titre de l'aticle" required autofocus>
+        </div>
         <br>
         <div class="form-group">
-            <label class="col-md-4 control-label">Ou entrez manuellement le nom d'un guide: </label>
-            <input name="nomGuideVisMan" class="form-control nomGuideVisMan" type="text" placeholder="Nom guide" value=''>
+            <label class="col-md-3 control-label">Lieu de la visite:</label>
+            <input name="lieuxVisite" class="form-control" type="text" placeholder="Lieux de la visite" required>
+        </div>
+        <div class="form-group">
+            <label class="col-md-3 control-label">Description de la visite: </label>
+            <input name='description' class="form-control" type="text" value="{{$description or ''}}" placeholder="Description de l'article" required autofocus>
+        </div>
+        <br>
+        <div class='form-group'>
+            <label class="col-md-3 control-label"> Date de la visite: </label>
+            <input name="date" class="form-control" type="date" value="" placeholder="Date et heure souhaitez pour la visite" required>
+        </div>
+        <br>
+        <div class="form-group">
+            <label class='col-md-3 control-label'>Prix de la visite: </label>
+            <input name='prix' class="form-control" type="number" placeholder="Prix de la visite" required>
+        </div>
+        <br>
+        <div class="form-group">
+            <label class="col-md-4 control-label">Nombre de place(s) pour la visite: </label>
+            <input name="nbPlace" class="form-control" type="number" placeholder="Nombre de place dispo pour cette visite" required>
+        </div>
+        <br>
+        <div class='form-group'>
+            <label class='col-md-4 control-label'>Sélectionnez le guide pour cette visite: </label>
+            <select name="nomGuideVisite" class="form-control nomGuideVis" required>
+                <option value="Selectionnez le guide souhaité" selected disabled>Selectionnez le guide souhaité</option>
+                @foreach($mesVisiteurs as $unVisiteur)
+                <option value="{{$unVisiteur -> idVis}}">{{$unVisiteur->prenomVis}} {{$unVisiteur->nomVis}}</option>
+                @endforeach
+            </select>
+            <br>
+            <div class="form-group">
+                <label class="col-md-4 control-label">Ou entrez manuellement le nom d'un guide: </label>
+                <input name="nomGuideVisMan" class="form-control nomGuideVisMan" type="text" placeholder="Nom guide" value='' required>
+            </div>
+        </div>
+        <br>
+        <div class="form-group">
+            <br><br>
+            <center> <button type="submit" class="btn btn-default btn-primary">
+                    <span class="glyphicon glyphicon-ok"></span> Valider
+                </button>
+                &nbsp;
+                <button type="button" class="btn btn-default btn-primary" 
+                        onclick="verifGuide();
+                                javascript: window.location = '{{url('/')}}';">
+                    <span class="glyphicon glyphicon-remove" ></span> Annuler</button> </center>
         </div>
     </div>
-    <br>
-    <div class="form-group">
-        <br><br>
-        <center> <button type="submit" class="btn btn-default btn-primary">
-                <span class="glyphicon glyphicon-ok"></span> Valider
-            </button>
-            &nbsp;
-            <button type="button" class="btn btn-default btn-primary" 
-                    onclick="verifGuide();javascript: window.location = '{{url('/acccueil')}}';">
-                <span class="glyphicon glyphicon-remove" ></span> Annuler</button> </center>
-    </div>
-</div>
-     </body>
-     
+</body>
+
 @stop
