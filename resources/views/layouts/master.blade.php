cf06d85150a62f01ade7239d0f3adb9625e14421
sio <sio@DESKTOP-GSEDVBB.sglo.priv>
2017-01-17 09:15:09 +0100

    Modification des redirections

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -44,7 +44,7 @@
 
     <body>
         <div class="brand">
-            <a href="{{url('/')}}"><img src="{{URL::asset('assets/image/logoTabagnon.png')}}" alt="Logo Tabagnon" height="123" width="100"></a>
+            <a href="{{url('/accueil')}}"><img src="{{URL::asset('assets/image/logoTabagnon.png')}}" alt="Logo Tabagnon" height="123" width="100"></a>
             Tabagnon | <small>Saint-Genis-les-Ollières</small></div>
         <!-- Navigation -->
         <nav class="navbar navbar-default" role="navigation">
