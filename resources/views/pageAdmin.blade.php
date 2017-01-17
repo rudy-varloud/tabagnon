93e4e72a8e4460e5d95f8b22cdfc8b1b82f97bba
sio <sio@DESKTOP-GSEDVBB.sglo.priv>
2017-01-17 12:13:36 +0100

    Modification pageAdmin / pageVisiteSpe

--- resources/views/pageAdmin.blade.php
+++ resources/views/pageAdmin.blade.php
@@ -1,3 +1,6 @@
 @extends('layouts.masterAdmin')
 @section('content')
+<div class='box'>
+    <p> Bienvenue sur le pannel administrateur du Tabagnon. Toute personnes n'ayant pas les droits </div>
+</div>
 @stop
